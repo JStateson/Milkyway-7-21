6702a9f86a7c1452644cc0ca6525877f340aaf68
Joseph Stateson <josephy@stateson.net>
2023-01-31 21:20:10 -0600

    added executable to project
    Code change to print a message indicating the the Milkyway fix is activated.

--- filter_7_21_cs_scheduler.cpp
+++ filter_7_21_cs_scheduler.cpp
@@ -180,8 +180,8 @@ int CLIENT_STATE::make_scheduler_request(PROJECT* p) {
         mw_low = MW_LOW_WATER_pct / 100.0;
         mw_high = MW_HIGH_WATER_pct / 100.0;
         bInitRatio = false;
-        if (bDebug)
-            msg_printf(0, MSG_INFO, "Using %d for low water and %d for high water pcts",
+        if (true)
+            msg_printf(0, MSG_INFO, "Milkyway fix: Using %d for low water and %d for high water pcts",
                 MW_LOW_WATER_pct, MW_HIGH_WATER_pct);
     }
 
